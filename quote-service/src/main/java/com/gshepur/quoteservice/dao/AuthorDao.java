57f901515eab6e26e8b1e87dcec9e35e1ccd6055
Goutam Shepur <goutam.s.h@gmail.com>
2019-03-06 23:57:34 +0000

    Initial Commit

---
+++ quote-service/src/main/java/com/gshepur/quoteservice/dao/AuthorDao.java
@@ -0,0 +1,7 @@
+package com.gshepur.quoteservice.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AuthorDao {
+}
--- quote-service/src/main/java/com/gshepur/quoteservice/service/AuthorService.java
+++ quote-service/src/main/java/com/gshepur/quoteservice/service/AuthorService.java
@@ -1,12 +1,17 @@
 package com.gshepur.quoteservice.service;
 
+import com.gshepur.quoteservice.dao.AuthorDao;
 import com.gshepur.quoteservice.model.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuthorService {
+
+    @Autowired
+    private AuthorDao authorDao;
 
     public List<Author> getAllAuthors() {
         return null;
