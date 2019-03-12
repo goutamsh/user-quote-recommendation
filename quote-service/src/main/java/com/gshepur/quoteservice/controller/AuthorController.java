21c06abeeb8d3644dfcf76a84bdf872e0b6b06c9
Goutam Shepur <goutam.s.h@gmail.com>
2019-03-12 01:58:14 +0000

    Logger support

--- quote-service/src/main/java/com/gshepur/quoteservice/controller/AuthorController.java
+++ quote-service/src/main/java/com/gshepur/quoteservice/controller/AuthorController.java
@@ -2,6 +2,10 @@ package com.gshepur.quoteservice.controller;
 
 import com.gshepur.entity.Author;
 import com.gshepur.quoteservice.service.AuthorService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +13,23 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("authors")
+@Api(value = "authors", description = "Operations pertaining to authors")
 public class AuthorController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthorController.class);
     @Autowired
     private AuthorService authorService;
 
+    @ApiOperation(value = "View a list of available products",response = Iterable.class)
     @GetMapping("/")
     public Iterable<Author> getAuthors(){
+        LOGGER.info("Request to fetch all authors");
         return authorService.getAllAuthors();
     }
 
     @GetMapping("/{id}")
     public Optional<Author> getAuthorById(@PathVariable int id){
+        LOGGER.info("Request to fetch  author by ID:{}", id);
         return authorService.getAuthorById(id);
     }
 
--- user-service/src/main/java/com/gshepur/userservice/controller/UserController.java
+++ user-service/src/main/java/com/gshepur/userservice/controller/UserController.java
@@ -3,6 +3,8 @@ package com.gshepur.userservice.controller;
 import com.gshepur.entity.User;
 import com.gshepur.userservice.dto.UserCategoryAddRequest;
 import com.gshepur.userservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +15,22 @@ import java.util.Optional;
 @RequestMapping("users")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
 
     @GetMapping(path = "/", produces = "application/json")
     public Iterable<User> getAllUsers(){
+        LOGGER.info("Request to fetch all users");
         return userService.getAllUsers();
     }
 
 
     @GetMapping(path = "/{id}", produces = "application/json")
     public Optional<User> getUserById(@PathVariable int id){
+        LOGGER.info("Request to fetch user by ID: {}", id);
         return userService.getUSerById(id);
     }
 
